84ad4cc386d5441aa14cd4a0b78b0e747d69c68b
aditya <aditya.kumar@qliktag.com>
2021-05-25 16:26:59 +0530

    completed reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactiveForm-sample';
+  signUpForm: FormGroup | any;
+  submitted = false;
+
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.signUpForm = this.fb.group({
+      fName: ['', Validators.required],
+      lName: ['', Validators.required],
+      addresses: this.fb.array([
+        this.initAddress(),
+      ]),
+      emailId: ['', [Validators.required, Validators.email]],
+      userPassword: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  initAddress() {
+    return this.fb.group({
+      street: ['', Validators.required],
+      postcode: ['', Validators.required, Validators.minLength(6)],
+      phonenumber: this.fb.array([
+        this.initNumber()
+      ])
+    });
+  }
+  initNumber() {
+    return this.fb.group({
+      number: ['', Validators.required]
+    })
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    console.log(this.signUpForm)
+
+    if (this.signUpForm.invalid) {
+      return;
+    }
+    console.log(JSON.stringify(this.signUpForm.value, null, 4))
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.signUpForm.reset();
+  }
 }
